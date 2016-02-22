a479d89bbd66f782fb7b1ef93e0778a95c9d0339
oldfly <yifei.z.luiz@gmail.com>
2016-02-22 10:32:51 -0300

    tra

--- loja/config.php
+++ loja/config.php
@@ -1,29 +1,29 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://www.rseml.com.br/reparosemlagrima/loja/');
+define('HTTP_SERVER', 'http://localhost/reparosemlagrima/loja/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://www.rseml.com.br/reparosemlagrima/loja/');
+define('HTTPS_SERVER', 'http://localhost/reparosemlagrima/loja/');
 
 // DIR
-define('DIR_APPLICATION', '/opt/lampp/htdocs/reparosemlagrima/loja/catalog/');
-define('DIR_SYSTEM', '/opt/lampp/htdocs/reparosemlagrima/loja/system/');
-define('DIR_LANGUAGE', '/opt/lampp/htdocs/reparosemlagrima/loja/catalog/language/');
-define('DIR_TEMPLATE', '/opt/lampp/htdocs/reparosemlagrima/loja/catalog/view/theme/');
-define('DIR_CONFIG', '/opt/lampp/htdocs/reparosemlagrima/loja/system/config/');
-define('DIR_IMAGE', '/opt/lampp/htdocs/reparosemlagrima/loja/image/');
-define('DIR_CACHE', '/opt/lampp/htdocs/reparosemlagrima/loja/system/storage/cache/');
-define('DIR_DOWNLOAD', '/opt/lampp/htdocs/reparosemlagrima/loja/system/storage/download/');
-define('DIR_LOGS', '/opt/lampp/htdocs/reparosemlagrima/loja/system/storage/logs/');
-define('DIR_MODIFICATION', '/opt/lampp/htdocs/reparosemlagrima/loja/system/storage/modification/');
-define('DIR_UPLOAD', '/opt/lampp/htdocs/reparosemlagrima/loja/system/storage/upload/');
+define('DIR_APPLICATION', 'C:/xampp/htdocs/reparosemlagrima/loja/catalog/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/reparosemlagrima/loja/system/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/reparosemlagrima/loja/catalog/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/reparosemlagrima/loja/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/reparosemlagrima/loja/system/config/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/reparosemlagrima/loja/image/');
+define('DIR_CACHE', 'C:/xampp/htdocs/reparosemlagrima/loja/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/reparosemlagrima/loja/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/htdocs/reparosemlagrima/loja/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/reparosemlagrima/loja/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/reparosemlagrima/loja/system/storage/upload/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
 //define('DB_USERNAME', 'usr_semlagrimas');
+define('DB_PASSWORD', '');
 //define('DB_PASSWORD', '8LRwGeh9');
 define('DB_DATABASE', 'bd_loja_rslagrimas');
 define('DB_PORT', '3306');
